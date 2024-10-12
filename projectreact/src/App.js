17764939c4ee2216855ad5fe5e03f01ea061bca3
Abir Hasan <abir.hasan@pickles.com.au>
2024-10-12 17:35:56 +0100

    Implement Props in The Tweet Box

--- projectreact/src/App.js
+++ projectreact/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import Hello from './sayHello';
 import Tweet from './tweet';
 
 function App(){
 
   return(
     <div className='app'>
-      <Hello/>
-      <Tweet/>
+      <Tweet name="Dev ED" message="This is a Random Tweet"/>
+      <Tweet name="John Snow" message="My new Course is Available"/>
+      <Tweet name="Winter is Coming" message="I'm the True King"/>
+      <Tweet name="Mosh" message="700K my dudes"/>
+
     </div>
   );
 }
