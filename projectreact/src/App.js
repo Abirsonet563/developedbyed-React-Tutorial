4204a5614b1e2951afc04e7352d751bd0b1a8365
Abir Hasan <abir.hasan@pickles.com.au>
2024-10-12 17:58:55 +0100

    Implemetn Toggle Using useState Hook

--- projectreact/src/App.js
+++ projectreact/src/App.js
@@ -2,13 +2,18 @@ import React, {useState} from 'react';
 import Tweet from './tweet';
 
 function App(){
+  const [red, setRed] = useState(false)
   const [count, setCount] = useState(10);
   function increment (){
    setCount(count + 1);
+  //  setRed(true)
+  //toggle
+  setRed(!red);
 }
   return(
     <div className='app'>
       <button onClick={increment}>Incrememnt</button>
+      <h1 className={red ? "red" : ""}>Red Color</h1>
       <p>{count}</p>
       <Tweet name="Mosh" message="700K my dudes"/>
 
